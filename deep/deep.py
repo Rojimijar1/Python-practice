0fac6a4e8a38fa6349a54755a70e8fa7b95ece51
Rojimijar1 <rojimijar@gmail.com>
2026-01-17 09:50:24 +0545

    deep code commit

---
+++ deep/deep.py
@@ -0,0 +1,9 @@
+#In deep.py, implement a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, outputting Yes
+#  if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No.
+
+#Deep-thought
+word=input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
+if word== '42' or word=="fourty-two" or word == "fourty two":
+    print("yes")
+else:
+    print("No")
